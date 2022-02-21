872cb173d59b037e37da0fe75f0ac9ab205798f6
Shaswat Misra <shaswatmisra2310@gmail.com>
2022-02-21 17:27:20 +0530

    commit message 2

---
+++ Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MedicalIntercomProject.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+       
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MedicalIntercomProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
